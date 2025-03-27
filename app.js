cd4bfcf43588fc5317aa3e34f8032d347a2af85a
MarcoVacchi <mvacchi816@gmail.com>
2025-03-27 14:58:36 +0100

    feat: delete comment

--- app.js
+++ app.js
@@ -5,14 +5,7 @@ const port = 3000;
 
 app.use(express.static("public"));
 
-//router 
 app.use("/posts", posts);
-
-// app.get("/posts", (req, res) => {
-//     console.log("Server del mio blog");
-//     res.send(posts);
-// });
-// //console.log(posts);
 
 app.listen(port, () => {
 console.log("sono un server attivo sulla porta"+ " " + port);
