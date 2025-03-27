22a4b5c14c02100d363aace033f0a4705b1f07c0
MarcoVacchi <mvacchi816@gmail.com>
2025-03-27 20:34:43 +0100

    feat: added array file

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
+const router = require("./router/posts");
 const posts = require("./router/posts");
 const express = require("express");
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
-
 app.use("/posts", posts);
+
 
 app.listen(port, () => {
 console.log("sono un server attivo sulla porta"+ " " + port);
